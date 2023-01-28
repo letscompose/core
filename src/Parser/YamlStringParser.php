0cb343cab519a36cad38091a6d10b34ad714d602
Igor ZLOBINE <izlobine@gmail.com>
2023-01-28 07:17:27 +0100

    yaml parser: add extends behavior

--- src/Parser/YamlStringParser.php
+++ src/Parser/YamlStringParser.php
@@ -12,17 +12,29 @@ namespace LetsCompose\Core\Parser;
 
 use LetsCompose\Core\Exception\ExceptionInterface;
 use LetsCompose\Core\Exception\InvalidArgumentException;
+use LetsCompose\Core\FileLoader\FileLoaderInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class YamlStringParser implements StringParserInterface
 {
     /**
      * @var StringPlaceholderResolverInterface[]
      */
-    protected array $paramsResolvers = [];
+    protected array $placeHolderResolvers = [];
 
-    public function __construct()
+    protected FileLoaderInterface $fileLoader;
+
+    protected const PARSER_BEHAVIOR_MAIN_SECTION = 'parser-behavior';
+
+    protected const PARSER_BEHAVIOR_EXTENDS_SECTION = 'extends';
+
+    protected const PARSER_BEHAVIOR_IMPORT_SECTION = 'imports';
+
+
+    public function __construct(FileLoaderInterface $fileLoader)
     {
-        $this->paramsResolvers = $this->getPlaceholderResolvers();
+        $this->placeHolderResolvers = $this->getPlaceholderResolvers();
+        $this->fileLoader = $fileLoader;
     }
 
     /**
@@ -31,26 +43,84 @@ class YamlStringParser implements StringParserInterface
      */
     public function parse(string $content, ?array $contentPlaceholderParameters = []): array
     {
-        foreach ($this->paramsResolvers as $resolver)
+
+        $content = $this->parseContent($content);
+        $content = json_encode($content);
+
+        foreach ($this->placeHolderResolvers as $resolver)
         {
             $resolver->setPlaceholdersNameValueList($contentPlaceholderParameters);
             $content = $resolver->resolvePlaceholders($content);
         }
 
+        return json_decode($content, true);
+    }
+
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ExceptionInterface
+     */
+    private function parseContent(string $content): array
+    {
+        $content = Yaml::parse($content);
+        return $this->processBehavior($content);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws ExceptionInterface
+     */
+    protected function processBehavior(array $content): array
+    {
+        // check if content has behavior section to process
+        $behaviorSection = $content[static::PARSER_BEHAVIOR_MAIN_SECTION] ?? false;
+        if (!$behaviorSection)
+        {
+            return $content;
+        }
+
         // process extends section
+        $content = $this->processExtends($behaviorSection, $content);
+
 
         // process import section
 
-
-        dump($content);
-        die();
         return $content;
 
     }
 
+    /**
+     * @throws InvalidArgumentException
+     * @throws ExceptionInterface
+     */
+    protected function processExtends(array $behaviorSection, array $content): array
+    {
+        $extendsSection = $behaviorSection[static::PARSER_BEHAVIOR_EXTENDS_SECTION] ?? false;
+        if (!$extendsSection)
+        {
+            return $content;
+        }
+
+        foreach ($extendsSection as $file)
+        {
+            $extendContent = $this->fileLoader->getContents($file);
+            $extendContent = $this->parseContent($extendContent);
+            $content = array_replace_recursive($extendContent, $content);
+        }
+
+        return $content;
+    }
+
+    protected function processImports(array $content): array
+    {
+        return $content;
+    }
+
+
     public function addParamResolver(StringPlaceholderResolverInterface $paramsResolver)
     {
-        $this->paramsResolvers[] = $paramsResolver;
+        $this->placeHolderResolvers[] = $paramsResolver;
     }
 
     public function getPlaceholderResolvers(): array
